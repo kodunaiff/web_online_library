129a537dbaaa2d8237844283646c105a9fd473c9
kodunaiff <kodunaiff@gmail.com>
2024-01-15 23:51:09 +0800

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,34 +1,37 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
-from urllib.parse import urlsplit
-from dotenv import load_dotenv
 import os
+from urllib.parse import urlsplit
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
+from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
 load_dotenv()
-library_path = os.getenv('LIBRARY_PATH')
-with open(f"{library_path}\\library.json", "r", encoding="utf8") as my_file:
-    library_json = my_file.read()
-library = json.loads(library_json)
-
-for book in library:
-    image_link = urlsplit(book["image_link"])
-    image_url = image_link.path.split('/')[2]
-    book['image_link'] = f'library_folder/{image_url}'
+library_path = os.environ['LIBRARY_PATH']
 
 
-rendered_page = template.render(library=library)
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open(f"{library_path}\\library.json", "r", encoding="utf8") as my_file:
+        library_json = my_file.read()
+    library = json.loads(library_json)
+    for book in library:
+        image_link = urlsplit(book["image_link"])
+        image_url = image_link.path.split('/')[2]
+        book['image_link'] = f'library_folder/{image_url}'
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    rendered_page = template.render(library=library)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+on_reload()
+
+server = Server()
+server.watch('template.html', on_reload)
+server.serve(root='.')
