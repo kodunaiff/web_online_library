8e927e2dcfd34ab193eacf195f81bc5f6b62b898
kodunaiff <kodunaiff@gmail.com>
2024-01-26 00:43:22 +0800

    fix code

--- render_website.py
+++ render_website.py
@@ -1,15 +1,12 @@
 import json
+import math
 import os
 from urllib.parse import urlsplit
 
-from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
-import math
 
-load_dotenv()
-library_path = os.environ['LIBRARY_PATH']
 os.makedirs('pages', exist_ok=True)
 
 
@@ -19,19 +16,20 @@ def on_reload():
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
+    library_path = os.path.join('media', 'library.json')
 
-    with open(f"{library_path}\\library.json", "r", encoding="utf8") as my_file:
+    with open(library_path, "r", encoding="utf8") as my_file:
         library_json = my_file.read()
     library = json.loads(library_json)
     for book in library:
         image_link = urlsplit(book["image_link"])
         image_url = image_link.path.split('/')[2]
-        book['image_link'] = os.path.join('library_folder', image_url)
-        book['book_path'] = os.path.join('library_folder', f"{book['book_name']}.txt")
+        book['image_link'] = os.path.join('media', image_url)
+        book['book_path'] = os.path.join('media', f"{book['book_name']}.txt")
 
     page_quantity_books = 10
     pages = chunked(library, page_quantity_books)
-    quantity_pages = math.ceil(len(library)/page_quantity_books)
+    quantity_pages = math.ceil(len(library) / page_quantity_books)
     for page_number, page in enumerate(pages, 1):
         rendered_page = template.render(library=chunked(page, 2),
                                         quantity_pages=quantity_pages,
@@ -39,7 +37,6 @@ def on_reload():
         page_path = os.path.join('pages', f'index{page_number}.html')
         with open(page_path, 'w', encoding="utf8") as file:
             file.write(rendered_page)
-
 
 
 on_reload()
